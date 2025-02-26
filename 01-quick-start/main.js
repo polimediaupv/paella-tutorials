8bae3b510420354a69e4b7bed94e8f1278ca18d5
Fernando Serrano Carpena <ferserc1@gmail.com>
2025-02-26 12:58:16 +0100

    Quick start tutorial updated to paella-core 2

--- 01-quick-start/main.js
+++ 01-quick-start/main.js
@@ -2,7 +2,7 @@ import { Paella } from '@asicupv/paella-core';
 
 import "@asicupv/paella-core/paella-core.css";
 
-const player = new Paella('player-container');
+const player = new Paella('playerContainer');
 
 const videoUrl = "https://repository.paellaplayer.upv.es/belmar-multiresolution/media/480-presenter.mp4";
 const preview = "https://repository.paellaplayer.upv.es/belmar-multiresolution/preview/belmar-preview.jpg";
