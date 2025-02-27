dc91aa710dda9895224c94f36d2c95f2da7a039d
Fernando Serrano Carpena <ferserc1@gmail.com>
2025-02-27 14:02:18 +0100

    Dual video example updated

--- 02-two-streams/main.js
+++ 02-two-streams/main.js
@@ -2,6 +2,6 @@ import { Paella } from '@asicupv/paella-core';
 
 import "@asicupv/paella-core/paella-core.css";
 
-const player = new Paella('player-container');
+const player = new Paella('playerContainer');
 
 await player.loadManifest();
