8450e4621e597c02fac97b2c2ba02e138c305d94
Fernando Serrano Carpena <ferserc1@gmail.com>
2025-02-28 12:14:00 +0100

    Initialisation parameters tutorial updated

--- 03-init-params/main.js
+++ 03-init-params/main.js
@@ -1,13 +1,28 @@
 import { 
-    Paella
-} from 'paella-core';
+    Paella,
+    defaultLoadConfigFunction,
+    utils
+} from '@asicupv/paella-core';
+import '@asicupv/paella-core/paella-core.css';
 
 const initParams = {
     // Initialization parameters
     configResourcesUrl: 'settings/',
     configUrl: 'settings/settings.json',
-    defaultVideoPreview: "/settings/default_preview_landscape.jpg"
+    defaultVideoPreview: "/settings/default_preview_landscape.jpg",
+
+    loadConfig: async (configUrl, player) => {
+        const config = await defaultLoadConfigFunction(configUrl, player);
+        utils.mergeObjects(config, {
+            plugins: {
+                "es.upv.paella.playPauseButton": {
+                    enabled: true
+                }
+            }
+        });
+        return config;
+    }
 };
-const player = new Paella('player-container', initParams);
+const player = new Paella('playerContainer', initParams);
 
 await player.loadManifest();
