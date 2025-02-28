d39af9ae6931e01a40b71bb56d5be73db6e43423
Fernando Serrano Carpena <ferserc1@gmail.com>
2025-02-28 14:31:32 +0100

    plugins tutorial updated

--- 04-plugins/main.js
+++ 04-plugins/main.js
@@ -1,22 +1,38 @@
 import { 
     Paella, 
+    defaultLoadConfigFunction,
+    utils,
     PlayPauseButtonPlugin
-} from 'paella-core';
+} from '@asicupv/paella-core';
 import {
     basicPlugins
-} from 'paella-basic-plugins';
+} from '@asicupv/paella-basic-plugins';
+
+import '@asicupv/paella-core/paella-core.css';
+import '@asicupv/paella-basic-plugins/paella-basic-plugins.css';
 
 const initParams = {
     // Initialization parameters
     configResourcesUrl: 'settings/',
     configUrl: 'settings/settings.json',
     defaultVideoPreview: "/settings/default_preview_landscape.jpg",
+    loadConfig: async (configUrl, player) => {
+        const config = await defaultLoadConfigFunction(configUrl, player);
+        utils.mergeObjects(config, {
+            plugins: {
+                "es.upv.paella.playPauseButton": {
+                    enabled: true
+                }
+            }
+        });
+        return config;
+    },
 
     plugins: [
         PlayPauseButtonPlugin,
         ...basicPlugins
     ]
 };
-const player = new Paella('player-container', initParams);
+const player = new Paella('playerContainer', initParams);
 
 await player.loadManifest();
